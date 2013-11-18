f5dece2e833252e08c298371cf34506c163a9831
claudiafeijo <claudiafeijo@.com>
2013-11-18 15:56:22 -0200

    feat(caverna): Adicione três tuneis cria camara

--- src/caverna.py
+++ src/caverna.py
@@ -30,44 +30,43 @@ class Caverna:
 
     def cria_caverna(self):
         """Cria a caverna e suas partes."""
-        self.camara + Camara(self.html)
-        tunel = self.html.DIV()
-        tunel.setAttribute('style', 'height:700; width: 33.33%; float:left;')
-        self.camara.div <=div()
-
-        self.main.style.width = 1000
-        self.main.style.height = 800
-        self.main.text = "Caverna da Claudia"
-
+        self.camara = Camara(self.html).cria_camara()
         # criando um tunel
         tunel = self.html.DIV()
-         # criando um tunel2
+        tunel.setAttribute('style', 'height:700; width: 33.33%; float:left;')
+        self.camara.div <=tunel
+        # criando um tunel2
         tunel2 = self.html.DIV()
         tunel2.setAttribute('style', 'height:700; width: 33.33%; float:left;')
-        self.main <=tunel2
+        self.camara.div <=tunel2
          # criando um tunel3
         tunel3 = self.html.DIV()
         tunel3.setAttribute('style', 'height:700; width: 33.33%; float:left;')
-        self.main <=tunel
-
+        self.camara.div <= tunel3
+        self.main <= self.camara.div
         return self
 
-class Camara:
-    """Uma camara de caverna com tuneis e habitantes, :ref:'camara'
-    """
-    def_init_(self,html):
-       """Inicia a camara. """
-       self.html + html
-       self.div + none
-       self.tunel + ()
 
-    def cria_caverna(self):
-        """Cria a caverna e suas partes."""
-        self.main.style.backgroundSize = 'cover'
-        self.main.style.backgroundImage = 'url(%s)' % CAVEX
-        self.main.style.width = 1000
-        self.main.style.height = 800
-        self.main.text = "Caverna da Claudia"
+class Camara:
+    """Uma camara de caverna com tuneis e habitantes. :ref:'camara'
+    """
+    def __init__(self, html):
+        """Inicia a camara. """
+        self.html = html
+        self.div = None
+        self.tunel = ()
+
+    def cria_camara(self):
+        """Cria a camara e suas partes."""
+        self.div = self.html. DIV()
+        self.div.style.backgroundSize = 'cover'
+        self.div.style.backgroundImage = 'url(%s)' % CAVEX
+        self.div.style.width = 1000
+        self.div.style.height = 800
+        self.div.text = "Caverna da Claudia"
+        return self
+
+
 def main(gui):
     print('Caverna 0.1.0')
     caverna = Caverna(gui).cria_caverna()
